c43b14bbcb60e130eae2ec0ee0802d5d4e1b7056
Rinchen Lama <rinchenlama0075@gmail.com>
2021-03-06 20:25:59 -0500

    none

--- speech_recog/FactsViewController.swift
+++ speech_recog/FactsViewController.swift
@@ -12,9 +12,7 @@ import UIKit
 import Parse
 
 class FactsViewController: UIViewController {
-//    let url = "https://factchecktools.googleapis.com"
-//    let search = "/v1alpha1/claims:search"
-//    let api_key = "AIzaSyBkTsZH8lbH6KDhHJB5BJ8E5X-9jMmqRhc"
+
     
     @IBOutlet weak var transcriptLabel: UILabel!
     let voiceOverlay = VoiceOverlayController()
@@ -45,22 +43,7 @@ class FactsViewController: UIViewController {
         super.viewDidLoad()
         
         
-      
-//        let url = URL(string: "https://factchecktools.googleapis.com/v1alpha1/claims:search/api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
-//        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
-//        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
-//        let task = session.dataTask(with: request) { (data, response, error) in
-//           // This will run when the network request returns
-//           if let error = error {
-//              print(error.localizedDescription)
-//           } else if let data = data {
-//              let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
-//                print(dataDictionary)
-              // TODO: Get the array of movies
-              // TODO: Store the movies in a property to use elsewhere
-              // TODO: Reload your table view data
-
-//           }
+    
         }
 //        task.resume()
 //
